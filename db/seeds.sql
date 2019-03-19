dd9891839d354ea39818d1bb3149de9f21972f07
Santiago tulande <santiagotulande@gmail.com>
2019-03-18 21:03:03 -0500

    change

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO burgers (burger_name,devoured)
+VALUES ("Cheeseburger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", TRUE);
